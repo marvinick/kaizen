e1e8810272c66f52f270e010f620aef1638981d9
marvinick <marviluw@gmail.com>
2015-05-30 16:42:26 -0500

    started dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
 class PagesController < ApplicationController
+    before_action :authenticate_user!, only: [:dashboard]
+
   def home
+    if current_user
+      redirect_to dashboard_path
+    end
   end
 
-  def delete
+  def dashboard
+    @user = current_user
+    @items = @user.items
+    @comments = @user.comments
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'pages#home'
+  get 'dashboard', to: "pages#dashboard"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
